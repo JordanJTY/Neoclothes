7ad0a695ca31540bc4f2ff428c4098efde2672c3
JordanJTY <darkjordanrayo@gmail.com>
2023-05-04 02:21:58 +0100

    Some errors fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,10 @@ import { DesignComponent } from './page/design/design.component';
 import { CollectionComponent } from './page/collection/collection.component';
 
 const routes: Routes = [
-  {path: '/home', component: HomeComponent },
-  {path: '/collection', component: CollectionComponent },
-  {path: '/designing', component: DesignComponent },
-  {path: '/about-us', component: HomeComponent },
+  {path: 'home', component: HomeComponent },
+  {path: 'collection', component: CollectionComponent },
+  {path: 'designing', component: DesignComponent },
+  {path: 'about-us', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
